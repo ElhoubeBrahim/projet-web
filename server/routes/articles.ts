147d11fb5babc8814c2a6b41d09fa81131b4026b
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-05-16 23:26:43 +0100

    Create http tests for routes

--- server/routes/articles.ts
+++ server/routes/articles.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import ArticlesController from "../controllers/articles";
+import CommentsController from "../controllers/comments";
 
 const articlesRoutes = express.Router();
 
@@ -8,5 +9,8 @@ articlesRoutes.post("/", ArticlesController.create);
 articlesRoutes.get("/:id", ArticlesController.show);
 articlesRoutes.patch("/:id", ArticlesController.update);
 articlesRoutes.delete("/:id", ArticlesController.delete);
+
+articlesRoutes.get("/:id/comments", CommentsController.index);
+articlesRoutes.post("/:id/comments", CommentsController.create);
 
 export { articlesRoutes };
--- server/routes/comments.ts
+++ server/routes/comments.ts
@@ -3,8 +3,6 @@ import CommentsController from "../controllers/comments";
 
 const commentsRoutes = express.Router();
 
-commentsRoutes.get("/", CommentsController.index);
-commentsRoutes.post("/", CommentsController.create);
 commentsRoutes.get("/:id", CommentsController.show);
 commentsRoutes.patch("/:id", CommentsController.update);
 commentsRoutes.delete("/:id", CommentsController.delete);
