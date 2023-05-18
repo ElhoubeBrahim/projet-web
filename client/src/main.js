3d371affba66ca55bd300447d2d7a75fcff1ef68
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-05-18 19:24:55 +0100

    Move config folder into src

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import "./style.css";
 import App from "./App.vue";
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import "../config/fontawesome";
-import "../config/axios";
+import "./config/fontawesome";
+import "./config/axios";
 
 createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
