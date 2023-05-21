e705fcc1a5f875d0729cf4a1c921df0659f168e6
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-05-21 18:46:10 +0100

    Implement input validation error display mechanism

--- client/src/components/InputField.vue
+++ client/src/components/InputField.vue
@@ -1,7 +1,7 @@
 <script>
 export default {
   name: "InputField",
-  props: ["type", "placeholder", "icon", "modelValue"],
+  props: ["type", "placeholder", "icon", "error", "modelValue"],
   methods: {
     handleInput(event) {
       this.$emit("update:modelValue", event.target.value);
@@ -11,17 +11,21 @@ export default {
 </script>
 
 <template>
-  <div class="relative mb-4">
-    <input
-      :type="type"
-      :placeholder="placeholder"
-      class="px-4 py-2 bg-primary outline-none border-none pl-10 text-secondary w-full placeholder:text-secondary"
-      :value="modelValue"
-      @input="handleInput"
-    />
-    <font-awesome-icon
-      :icon="icon"
-      class="absolute top-0 left-0 mt-3 ml-3 text-secondary"
-    />
+  <div class="mb-4">
+    <div class="relative">
+      <input
+        :type="type"
+        :placeholder="placeholder"
+        class="px-4 py-2 bg-primary outline-none pl-10 text-secondary w-full placeholder:text-secondary"
+        :class="{ 'border border-red-500': error }"
+        :value="modelValue"
+        @input="handleInput"
+      />
+      <font-awesome-icon
+        :icon="icon"
+        class="absolute top-0 left-0 mt-3 ml-3 text-secondary"
+      />
+    </div>
+    <div class="text-red-500 text-xs mt-1" v-if="error">{{ error }}</div>
   </div>
 </template>
