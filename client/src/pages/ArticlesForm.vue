c736ce0e2d86063b52f3becc6149eaa38cbd374c
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-05-21 19:43:08 +0100

    Enhance error handling for article form

--- client/src/pages/ArticlesForm.vue
+++ client/src/pages/ArticlesForm.vue
@@ -53,8 +53,10 @@ export default {
         let article = null;
         if (this.isEdit) {
           article = await updateArticle(this.article.id, data);
+          this.$toast.success("Article updated successfully");
         } else {
           article = await createArticle(data);
+          this.$toast.success("Article created successfully");
         }
 
         if (this.image && article) {
@@ -63,7 +65,11 @@ export default {
 
         this.$router.push(`/read/${article.id}`);
       } catch (error) {
-        console.log(error);
+        for (const key in error.response.data.errors) {
+          this.errors[key] = error.response.data.errors[key][0];
+        }
+
+        this.$toast.error("Something went wrong!");
       }
 
       this.loading = false;
@@ -121,11 +127,13 @@ export default {
           placeholder="Article Title"
           icon="user"
           v-model="article.title"
+          :error="errors.title"
         />
         <div class="mb-4">
           <select
             v-model="article.category"
-            class="px-4 py-2 bg-primary outline-none border-none text-secondary w-full placeholder:text-secondary"
+            class="px-4 py-2 bg-primary outline-none text-secondary w-full placeholder:text-secondary"
+            :class="{ 'border border-red-500': errors.categoryIds }"
           >
             <option :value="null">Choose category</option>
             <option
@@ -136,13 +144,19 @@ export default {
               {{ category.name }}
             </option>
           </select>
+          <div class="text-red-500 text-xs mt-1">{{ errors.categoryIds }}</div>
         </div>
         <div class="relative mb-4">
           <textarea
             placeholder="Article Content"
             v-model="article.content"
-            class="px-4 py-2 bg-primary outline-none border-none text-secondary w-full placeholder:text-secondary h-[300px]"
+            class="px-4 py-2 bg-primary outline-none text-secondary w-full placeholder:text-secondary h-[300px]"
+            :class="{ 'border border-red-500': errors.content }"
           ></textarea>
+          <div class="absolute bottom-2 right-2 text-xs text-gray-500">
+            {{ article.content.length }} / 5000
+          </div>
+          <div class="text-red-500 text-xs mt-1">{{ errors.content }}</div>
         </div>
         <div class="mb-4">
           <input
