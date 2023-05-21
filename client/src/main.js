c5597b407e69ced4a2c3ce618fca7a9ffe4a7f30
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-05-21 18:45:34 +0100

    Integrate vue-toast-notification for enhanced user notifications

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { Skeletor } from "vue-skeletor";
+import ToastPlugin from "vue-toast-notification";
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import "./config/fontawesome";
@@ -10,6 +12,7 @@ import router from "./config/routes";
 
 createApp(App)
   .use(router)
+  .use(ToastPlugin)
   .component("skeletor", Skeletor)
   .component("font-awesome-icon", FontAwesomeIcon)
   .mount("#app");
