3d11a86ef94070459fbadcfce49d4fdee7d7820b
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-05-21 18:46:42 +0100

    Enhance error handling for comments form

--- client/src/components/CommentsModal.vue
+++ client/src/components/CommentsModal.vue
@@ -13,16 +13,37 @@ export default {
         email: "",
         content: "",
       },
+      errors: {},
       loading: false,
     };
   },
   methods: {
     async postComment() {
+      this.errors = {};
       this.loading = true;
-      const newComment = await createComment(this.article.id, this.comment);
-      this.$emit("commentCreated", newComment);
+
+      try {
+        const newComment = await createComment(this.article.id, this.comment);
+        this.$emit("commentCreated", newComment);
+        this.$toast.success("Comment posted successfully");
+        this.reset();
+      } catch (error) {
+        for (const key in error.response.data.errors) {
+          this.errors[key] = error.response.data.errors[key][0];
+        }
+        this.$toast.error("Something went wrong");
+      }
+
       this.loading = false;
     },
+    reset() {
+      this.comment = {
+        name: "",
+        email: "",
+        content: "",
+      };
+      this.errors = {};
+    }
   },
   components: { CommentCard, InputField },
 };
@@ -31,7 +52,12 @@ export default {
 <template>
   <div
     class="fixed top-0 left-0 bottom-0 right-0 h-full w-full bg-[#00000080] overflow-auto"
-    @click.self="close()"
+    @click.self="
+      () => {
+        reset();
+        close();
+      }
+    "
     v-if="open"
   >
     <div class="bg-white mx-4 my-10 lg:mx-auto lg:w-[80%]">
@@ -43,24 +69,32 @@ export default {
             placeholder="username"
             icon="user"
             v-model="comment.name"
+            :error="errors.name"
           />
           <InputField
             type="email"
             placeholder="example@example.com"
             icon="envelope"
             v-model="comment.email"
+            :error="errors.email"
           />
         </div>
-        <div class="relative mb-4">
-          <textarea
-            placeholder="comment"
-            v-model="comment.content"
-            class="px-4 py-2 bg-primary outline-none border-none pl-10 text-secondary w-full placeholder:text-secondary h-[300px]"
-          ></textarea>
-          <font-awesome-icon
-            icon="comment"
-            class="absolute top-3 left-4 transform text-secondary"
-          />
+        <div class="mb-4">
+          <div class="relative">
+            <textarea
+              placeholder="comment"
+              v-model="comment.content"
+              class="px-4 py-2 bg-primary outline-none pl-10 text-secondary w-full placeholder:text-secondary h-[300px]"
+              :class="{ 'border border-red-500': errors.content }"
+            ></textarea>
+            <font-awesome-icon
+              icon="comment"
+              class="absolute top-3 left-4 transform text-secondary"
+            />
+          </div>
+          <div class="text-red-500 text-xs mt-1" v-if="errors.content">
+            {{ errors.content }}
+          </div>
         </div>
         <button
           class="bg-secondary px-4 py-2 text-white disabled:opacity-50 disabled:cursor-not-allowed"
